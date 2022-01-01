0531b708758867d5630d575bfbe8ba6cd02eb9cd
amoukhtari <amoukhtari@pm.me>
2022-01-01 11:49:37 +0100

    Show available categories in dropdown menu

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from .models import Category, Product
 
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
 def products_all(request):
     products = Product.objects.all()
     return render(request, 'catalogue/home.html', {'products': products})
